cf93c9d345b25ec5f40c860ef5a108b96f7f6eb2
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-05-19 19:51:06 +0400

    feat: портировать раздел О нас в JSX

---
+++ src/components/AboutCard/AboutCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function AboutCard(props) {
+  const {
+    title, text, path, link,
+  } = props;
+
+  return (
+    <article className="card-container about__card-container">
+      <div className="card card_color_green about__card">
+        <h2 className="section-title">{title}</h2>
+      </div>
+      <div className="card card_content_annotation">
+        <div className="card__content about__card-content">
+          <div className="card__annotation">
+            <p className="paragraph card__paragraph">{text}</p>
+          </div>
+        </div>
+        <Link to={path} className="link card__link about__card-link">
+          {link}
+        </Link>
+      </div>
+    </article>
+  );
+}
+
+AboutCard.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  path: PropTypes.string,
+  link: PropTypes.string,
+};
+
+export default AboutCard;
