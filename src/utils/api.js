a67a43d4c3dfba7661e4a2c42ebab50bc08e4731
Evgeniya Zubova <ev.zu8ova@yandex.ru>
2021-05-19 14:37:17 +0400

    chore: эмулировать сервер

---
+++ src/utils/api.js
@@ -0,0 +1,42 @@
+// Импортируем пакет axios
+import axios from 'axios';
+
+class Api {
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://localhost:8000',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+  }
+
+  authUser() {
+    return this.instance.post('/token');
+  }
+
+  getCities() {
+    return this.instance.get('/cities');
+  }
+
+  updateProfile() {
+    return this.instance.get('/profile');
+  }
+
+  getMain() {
+    return this.instance.get('/main');
+  }
+
+  getEvents() {
+    return this.instance.get('/afisha/events');
+  }
+
+  bookEvent() {
+    return this.instance.post('/afisha/event-participants');
+  }
+}
+
+// Создание экземпляра класса Api
+const api = new Api();
+
+export default api;
