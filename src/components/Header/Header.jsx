5bda6a43a5ff25385f28ad8374561e3a1cb7a239
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 15:54:14 +0300

    feat: вынести ссылки из Header в HeaderLinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import HeaderLinks from '../HeaderLinks/HeaderLinks';
+
 import recentResults from '../../assets/dev-data/search-results'; // Временный массив с готовыми результатами поиска
 
 function Header() {
@@ -48,109 +50,10 @@ function Header() {
         <NavLink className="menu__logo" to="/">
           наставники.про
         </NavLink>
-        <div className={menuListsWrapClass}>
-          <ul className="menu__list">
-            <li className="menu__list-item">
-              <NavLink className="menu__link" to="/">
-                Календарь
-              </NavLink>
-            </li>
-            <li className="menu__list-item">
-              <NavLink className="menu__link" to="/">
-                Куда пойти
-              </NavLink>
-            </li>
-            <li className="menu__list-item">
-              <NavLink className="menu__link" to="/">
-                Вопросы
-              </NavLink>
-            </li>
-            <li className="menu__list-item menu__dropdown-item">
-              <NavLink className="menu__link" to="/">
-                Читать и смотреть
-              </NavLink>
-              <ul className="menu__dropdown-list">
-                <li className="menu__dropdown-list-item">
-                  <NavLink className="link menu__dropdown-link" to="/">
-                    Справочник
-                  </NavLink>
-                </li>
-                <li className="menu__dropdown-list-item">
-                  <NavLink className="link menu__dropdown-link" to="/">
-                    Видео
-                  </NavLink>
-                </li>
-                <li className="menu__dropdown-list-item">
-                  <NavLink className="link menu__dropdown-link" to="/">
-                    Статьи
-                  </NavLink>
-                </li>
-                <li className="menu__dropdown-list-item">
-                  <NavLink className="link menu__dropdown-link" to="/">
-                    Фильмы
-                  </NavLink>
-                </li>
-                <li className="menu__dropdown-list-item">
-                  <NavLink className="link menu__dropdown-link" to="/">
-                    Книги
-                  </NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="menu__list-item">
-              <NavLink className="menu__link" to="/">
-                Права детей
-              </NavLink>
-            </li>
-            <li className="menu__list-item">
-              <NavLink className="menu__link" to="/">
-                Истории
-              </NavLink>
-            </li>
-          </ul>
-          <ul className={menuListSocialClass}>
-            <li className="menu__list-item">
-              <a
-                className="menu__link"
-                href="https://www.facebook.com/BigBrothers.BigSisters.Russia/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                facebook
-              </a>
-            </li>
-            <li className="menu__list-item">
-              <a
-                className="menu__link"
-                href="https://vk.com/big.brothers.big.sisters"
-                target="_blank"
-                rel="noreferrer"
-              >
-                vkontakte
-              </a>
-            </li>
-            <li className="menu__list-item">
-              <a
-                className="menu__link"
-                href="https://www.instagram.com/nastavniki_org/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                instagram
-              </a>
-            </li>
-            <li className="menu__list-item">
-              <a
-                className="menu__link"
-                href="https://www.youtube.com/user/Nastavniki/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                youtube
-              </a>
-            </li>
-          </ul>
-        </div>
+        <HeaderLinks
+          menuListsWrapClass={menuListsWrapClass}
+          menuListSocialClass={menuListSocialClass}
+        />
         <button
           className={burgerMenuClass}
           type="button"
--- src/components/HeaderLinks/HeaderLinks.jsx
+++ src/components/HeaderLinks/HeaderLinks.jsx
@@ -1,7 +1,112 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function HeaderLinks() {
-  return;
+function HeaderLinks({ menuListsWrapClass, menuListSocialClass }) {
+  return (
+    <div className={menuListsWrapClass}>
+      <ul className="menu__list">
+        <li className="menu__list-item">
+          <NavLink className="menu__link" to="/">
+            Календарь
+          </NavLink>
+        </li>
+        <li className="menu__list-item">
+          <NavLink className="menu__link" to="/">
+            Куда пойти
+          </NavLink>
+        </li>
+        <li className="menu__list-item">
+          <NavLink className="menu__link" to="/">
+            Вопросы
+          </NavLink>
+        </li>
+        <li className="menu__list-item menu__dropdown-item">
+          <NavLink className="menu__link" to="/">
+            Читать и смотреть
+          </NavLink>
+          <ul className="menu__dropdown-list">
+            <li className="menu__dropdown-list-item">
+              <NavLink className="link menu__dropdown-link" to="/">
+                Справочник
+              </NavLink>
+            </li>
+            <li className="menu__dropdown-list-item">
+              <NavLink className="link menu__dropdown-link" to="/">
+                Видео
+              </NavLink>
+            </li>
+            <li className="menu__dropdown-list-item">
+              <NavLink className="link menu__dropdown-link" to="/">
+                Статьи
+              </NavLink>
+            </li>
+            <li className="menu__dropdown-list-item">
+              <NavLink className="link menu__dropdown-link" to="/">
+                Фильмы
+              </NavLink>
+            </li>
+            <li className="menu__dropdown-list-item">
+              <NavLink className="link menu__dropdown-link" to="/">
+                Книги
+              </NavLink>
+            </li>
+          </ul>
+        </li>
+        <li className="menu__list-item">
+          <NavLink className="menu__link" to="/">
+            Права детей
+          </NavLink>
+        </li>
+        <li className="menu__list-item">
+          <NavLink className="menu__link" to="/">
+            Истории
+          </NavLink>
+        </li>
+      </ul>
+      <ul className={menuListSocialClass}>
+        <li className="menu__list-item">
+          <a
+            className="menu__link"
+            href="https://www.facebook.com/BigBrothers.BigSisters.Russia/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            facebook
+          </a>
+        </li>
+        <li className="menu__list-item">
+          <a
+            className="menu__link"
+            href="https://vk.com/big.brothers.big.sisters"
+            target="_blank"
+            rel="noreferrer"
+          >
+            vkontakte
+          </a>
+        </li>
+        <li className="menu__list-item">
+          <a
+            className="menu__link"
+            href="https://www.instagram.com/nastavniki_org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            instagram
+          </a>
+        </li>
+        <li className="menu__list-item">
+          <a
+            className="menu__link"
+            href="https://www.youtube.com/user/Nastavniki/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            youtube
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default HeaderLinks;
