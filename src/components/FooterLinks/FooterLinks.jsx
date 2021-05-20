c4350e6c775ae2488121432468272f7812ef6d06
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 17:33:22 +0300

    refactor: вынести ссылки из FooterLinks в FooterRegularLinks

--- src/components/FooterLinks/FooterLinks.jsx
+++ src/components/FooterLinks/FooterLinks.jsx
@@ -1,48 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+
+import FooterRegularLinks from '../FooterRegularLinks/FooterRegularLinks';
 
 function FooterLinks() {
   return (
     <>
-      <nav className="footer__column footer__column_content_info">
-        <ul className="footer__column-list">
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              О проекте
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Календарь
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Куда пойти
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Вопросы
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Читать и смотреть
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Права детей
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Истории
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <FooterRegularLinks />
       <nav className="footer__column footer__column_content_social">
         <ul className="footer__column-list">
           <li className="footer__column-links">
--- src/components/FooterRegularLinks/FooterRegularLinks.jsx
+++ src/components/FooterRegularLinks/FooterRegularLinks.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function FooterRegularLinks() {
-  return;
+  return (
+    <nav className="footer__column footer__column_content_info">
+      <ul className="footer__column-list">
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            О проекте
+          </NavLink>
+        </li>
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            Календарь
+          </NavLink>
+        </li>
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            Куда пойти
+          </NavLink>
+        </li>
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            Вопросы
+          </NavLink>
+        </li>
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            Читать и смотреть
+          </NavLink>
+        </li>
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            Права детей
+          </NavLink>
+        </li>
+        <li className="footer__column-links">
+          <NavLink to="/" className="footer__column-link">
+            Истории
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default FooterRegularLinks;
