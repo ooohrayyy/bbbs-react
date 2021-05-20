bbe710ab8d014dcb27e4e1d92b8fcbebfce52d93
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 16:01:41 +0300

    refactor: вынести поиск из HeaderButtons в HeaderSearch

--- src/components/HeaderButtons/HeaderButtons.jsx
+++ src/components/HeaderButtons/HeaderButtons.jsx
@@ -1,44 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-import recentResults from '../../assets/dev-data/search-results'; // Временный массив с готовыми результатами поиска
+import HeaderSearch from '../HeaderSearch/HeaderSearch';
 
 function HeaderButtons({ toggleSearchMenu, searchOptionsClass }) {
   return (
     <ul className="menu__button-list">
       <li className="menu__button-item">
-        <form className="search" name="search-form">
-          <button
-            className="menu__button menu__button_type_search search__button"
-            type="button"
-            aria-label="Поиск"
-            title="Поиск"
-            onClick={toggleSearchMenu}
-          />
-          <div className={searchOptionsClass}>
-            <input
-              className="search__input paragraph"
-              type="text"
-              name="search"
-              placeholder="Поиск"
-            />
-            <ul className="search__option-list">
-              {recentResults.map((result, index) => (
-                <li className="search__option-item" key={index}>
-                  <NavLink
-                    className="search__title-link section-title section-title_clickable"
-                    to={result.link}
-                  >
-                    {result.title}
-                  </NavLink>
-                  <NavLink className="link search__link" to="/">
-                    {result.label}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </form>
+        <HeaderSearch
+          toggleSearchMenu={toggleSearchMenu}
+          searchOptionsClass={searchOptionsClass}
+        />
       </li>
       <li className="menu__button-item">
         <button
