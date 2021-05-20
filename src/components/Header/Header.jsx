2352b0c06d78cd6b987b2270d599f53cf3d76bef
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 15:58:35 +0300

    refactor: вынести кнопки из Header в HeaderButtons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import HeaderLinks from '../HeaderLinks/HeaderLinks';
-
-import recentResults from '../../assets/dev-data/search-results'; // Временный массив с готовыми результатами поиска
+import HeaderButtons from '../HeaderButtons/HeaderButtons';
 
 function Header() {
   const [burgerMenuisOpen, setBurgerMenuIsOpen] = React.useState(false);
@@ -63,50 +62,10 @@ function Header() {
           <span className="menu__burger-line" />
           <span className="menu__burger-line" />
         </button>
-        <ul className="menu__button-list">
-          <li className="menu__button-item">
-            <form className="search" name="search-form">
-              <button
-                className="menu__button menu__button_type_search search__button"
-                type="button"
-                aria-label="Поиск"
-                title="Поиск"
-                onClick={toggleSearchMenu}
-              />
-              <div className={searchOptionsClass}>
-                <input
-                  className="search__input paragraph"
-                  type="text"
-                  name="search"
-                  placeholder="Поиск"
-                />
-                <ul className="search__option-list">
-                  {recentResults.map((result, index) => (
-                    <li className="search__option-item" key={index}>
-                      <NavLink
-                        className="search__title-link section-title section-title_clickable"
-                        to={result.link}
-                      >
-                        {result.title}
-                      </NavLink>
-                      <NavLink className="link search__link" to="/">
-                        {result.label}
-                      </NavLink>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </form>
-          </li>
-          <li className="menu__button-item">
-            <button
-              className="menu__button menu__button_type_user"
-              type="button"
-              aria-label="Личный кабинет"
-              title="Личный кабинет"
-            />
-          </li>
-        </ul>
+        <HeaderButtons
+          toggleSearchMenu={toggleSearchMenu}
+          searchOptionsClass={searchOptionsClass}
+        />
       </nav>
     </header>
   );
--- src/components/HeaderButtons/HeaderButtons.jsx
+++ src/components/HeaderButtons/HeaderButtons.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function HeaderButtons() {
-  return;
+import recentResults from '../../assets/dev-data/search-results'; // Временный массив с готовыми результатами поиска
+
+function HeaderButtons({ toggleSearchMenu, searchOptionsClass }) {
+  return (
+    <ul className="menu__button-list">
+      <li className="menu__button-item">
+        <form className="search" name="search-form">
+          <button
+            className="menu__button menu__button_type_search search__button"
+            type="button"
+            aria-label="Поиск"
+            title="Поиск"
+            onClick={toggleSearchMenu}
+          />
+          <div className={searchOptionsClass}>
+            <input
+              className="search__input paragraph"
+              type="text"
+              name="search"
+              placeholder="Поиск"
+            />
+            <ul className="search__option-list">
+              {recentResults.map((result, index) => (
+                <li className="search__option-item" key={index}>
+                  <NavLink
+                    className="search__title-link section-title section-title_clickable"
+                    to={result.link}
+                  >
+                    {result.title}
+                  </NavLink>
+                  <NavLink className="link search__link" to="/">
+                    {result.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </form>
+      </li>
+      <li className="menu__button-item">
+        <button
+          className="menu__button menu__button_type_user"
+          type="button"
+          aria-label="Личный кабинет"
+          title="Личный кабинет"
+        />
+      </li>
+    </ul>
+  );
 }
 
 export default HeaderButtons;
