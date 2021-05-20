fccf601b8d1ba2c43c935730404e21da7ad27c49
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 17:35:01 +0300

    refactor: вынести ссылки из FooterLinks в FooterSocialLinks

--- src/components/FooterLinks/FooterLinks.jsx
+++ src/components/FooterLinks/FooterLinks.jsx
@@ -1,55 +1,13 @@
 import React from 'react';
 
 import FooterRegularLinks from '../FooterRegularLinks/FooterRegularLinks';
+import FooterSocialLinks from '../FooterSocialLinks/FooterSocialLinks';
 
 function FooterLinks() {
   return (
     <>
       <FooterRegularLinks />
-      <nav className="footer__column footer__column_content_social">
-        <ul className="footer__column-list">
-          <li className="footer__column-links">
-            <a
-              href="https://www.facebook.com/BigBrothers.BigSisters.Russia/"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Facebook
-            </a>
-          </li>
-          <li className="footer__column-links">
-            <a
-              href="https://vk.com/big.brothers.big.sisters"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Vkontakte
-            </a>
-          </li>
-          <li className="footer__column-links">
-            <a
-              href="https://www.instagram.com/nastavniki_org/"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Instagram
-            </a>
-          </li>
-          <li className="footer__column-links">
-            <a
-              href="https://www.youtube.com/user/Nastavniki/"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              YouTube
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <FooterSocialLinks />
     </>
   );
 }
--- src/components/FooterSocialLinks/FooterSocialLinks.jsx
+++ src/components/FooterSocialLinks/FooterSocialLinks.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
 
 function FooterSocialLinks() {
-  return;
+  return (
+    <nav className="footer__column footer__column_content_social">
+      <ul className="footer__column-list">
+        <li className="footer__column-links">
+          <a
+            href="https://www.facebook.com/BigBrothers.BigSisters.Russia/"
+            className="footer__column-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Facebook
+          </a>
+        </li>
+        <li className="footer__column-links">
+          <a
+            href="https://vk.com/big.brothers.big.sisters"
+            className="footer__column-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vkontakte
+          </a>
+        </li>
+        <li className="footer__column-links">
+          <a
+            href="https://www.instagram.com/nastavniki_org/"
+            className="footer__column-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+        <li className="footer__column-links">
+          <a
+            href="https://www.youtube.com/user/Nastavniki/"
+            className="footer__column-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            YouTube
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default FooterSocialLinks;
