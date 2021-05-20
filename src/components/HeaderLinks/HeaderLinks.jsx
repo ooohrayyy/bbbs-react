4af6f0812b5b8cf63d03a19d1381054cc8532d31
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 16:19:55 +0300

    refactor: вынести регулярные ссылки из HeaderLinks в HeaderRegularLinks

--- src/components/HeaderLinks/HeaderLinks.jsx
+++ src/components/HeaderLinks/HeaderLinks.jsx
@@ -1,70 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
+import HeaderRegularLinks from '../HeaderRegularLinks/HeaderRegularLinks';
 import HeaderSocialLinks from '../HeaderSocialLinks/HeaderSocialLinks';
 
 function HeaderLinks({ menuListsWrapClass, menuListSocialClass }) {
   return (
     <div className={menuListsWrapClass}>
-      <ul className="menu__list">
-        <li className="menu__list-item">
-          <NavLink className="menu__link" to="/">
-            Календарь
-          </NavLink>
-        </li>
-        <li className="menu__list-item">
-          <NavLink className="menu__link" to="/">
-            Куда пойти
-          </NavLink>
-        </li>
-        <li className="menu__list-item">
-          <NavLink className="menu__link" to="/">
-            Вопросы
-          </NavLink>
-        </li>
-        <li className="menu__list-item menu__dropdown-item">
-          <NavLink className="menu__link" to="/">
-            Читать и смотреть
-          </NavLink>
-          <ul className="menu__dropdown-list">
-            <li className="menu__dropdown-list-item">
-              <NavLink className="link menu__dropdown-link" to="/">
-                Справочник
-              </NavLink>
-            </li>
-            <li className="menu__dropdown-list-item">
-              <NavLink className="link menu__dropdown-link" to="/">
-                Видео
-              </NavLink>
-            </li>
-            <li className="menu__dropdown-list-item">
-              <NavLink className="link menu__dropdown-link" to="/">
-                Статьи
-              </NavLink>
-            </li>
-            <li className="menu__dropdown-list-item">
-              <NavLink className="link menu__dropdown-link" to="/">
-                Фильмы
-              </NavLink>
-            </li>
-            <li className="menu__dropdown-list-item">
-              <NavLink className="link menu__dropdown-link" to="/">
-                Книги
-              </NavLink>
-            </li>
-          </ul>
-        </li>
-        <li className="menu__list-item">
-          <NavLink className="menu__link" to="/">
-            Права детей
-          </NavLink>
-        </li>
-        <li className="menu__list-item">
-          <NavLink className="menu__link" to="/">
-            Истории
-          </NavLink>
-        </li>
-      </ul>
+      <HeaderRegularLinks />
       <HeaderSocialLinks menuListSocialClass={menuListSocialClass} />
     </div>
   );
