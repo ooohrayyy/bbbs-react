ccd0983cde478232951fe146d401a1eae8d5ef5c
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 17:38:16 +0300

    refactor: вынести контент из Footer в FooterMainContent

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,50 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
+import FooterMainContent from '../FooterMainContent/FooterMainContent';
 import FooterLinks from '../FooterLinks/FooterLinks';
-
-import logo from '../../images/svg/footer-logo.svg';
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <NavLink className="footer__logo" to="/">
-        <img
-          className="footer__logo-image"
-          src={logo}
-          alt="Логотип проекта «Старшие Братья Старшие Сёстры России»"
-        />
-      </NavLink>
-      <button className="button footer__button" type="button">
-        Помочь деньгами
-      </button>
-      <div className="footer__column footer__column_content_concept">
-        <p className="footer__brand">&copy; Старшие Братья Старшие Сёстры</p>
-        <div className="footer__copyright">
-          <p className="footer__authors">
-            Разработка – студенты
-            <a
-              href="https://praktikum.yandex.ru/"
-              className="footer__production"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Яндекс.Практикум
-            </a>
-          </p>
-          <p className="footer__design">
-            Концепция и дизайн –
-            <a
-              href="https://krkr.design/"
-              className="footer__production"
-              target="_blank"
-              rel="noreferrer"
-            >
-              krkr.design
-            </a>
-          </p>
-        </div>
-      </div>
+      <FooterMainContent />
       <FooterLinks />
     </footer>
   );
--- src/components/FooterMainContent/FooterMainContent.jsx
+++ src/components/FooterMainContent/FooterMainContent.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '../../images/svg/footer-logo.svg';
 
 function FooterMainContent() {
-  return;
+  return (
+    <>
+      <NavLink className="footer__logo" to="/">
+        <img
+          className="footer__logo-image"
+          src={logo}
+          alt="Логотип проекта «Старшие Братья Старшие Сёстры России»"
+        />
+      </NavLink>
+      <button className="button footer__button" type="button">
+        Помочь деньгами
+      </button>
+      <div className="footer__column footer__column_content_concept">
+        <p className="footer__brand">&copy; Старшие Братья Старшие Сёстры</p>
+        <div className="footer__copyright">
+          <p className="footer__authors">
+            Разработка – студенты
+            <a
+              href="https://praktikum.yandex.ru/"
+              className="footer__production"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Яндекс.Практикум
+            </a>
+          </p>
+          <p className="footer__design">
+            Концепция и дизайн –
+            <a
+              href="https://krkr.design/"
+              className="footer__production"
+              target="_blank"
+              rel="noreferrer"
+            >
+              krkr.design
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FooterMainContent;
