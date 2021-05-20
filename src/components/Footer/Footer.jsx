fdbbe4a27f77fdc41de908656f7e127b94f0338f
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-20 17:31:17 +0300

    refactor: вынести ссылки из Footer в FooterLinks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import FooterLinks from '../FooterLinks/FooterLinks';
+
 import logo from '../../images/svg/footer-logo.svg';
 
 export default function Footer() {
@@ -43,89 +45,7 @@ export default function Footer() {
           </p>
         </div>
       </div>
-      <nav className="footer__column footer__column_content_info">
-        <ul className="footer__column-list">
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              О проекте
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Календарь
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Куда пойти
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Вопросы
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Читать и смотреть
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Права детей
-            </NavLink>
-          </li>
-          <li className="footer__column-links">
-            <NavLink to="/" className="footer__column-link">
-              Истории
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <nav className="footer__column footer__column_content_social">
-        <ul className="footer__column-list">
-          <li className="footer__column-links">
-            <a
-              href="https://www.facebook.com/BigBrothers.BigSisters.Russia/"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Facebook
-            </a>
-          </li>
-          <li className="footer__column-links">
-            <a
-              href="https://vk.com/big.brothers.big.sisters"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Vkontakte
-            </a>
-          </li>
-          <li className="footer__column-links">
-            <a
-              href="https://www.instagram.com/nastavniki_org/"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Instagram
-            </a>
-          </li>
-          <li className="footer__column-links">
-            <a
-              href="https://www.youtube.com/user/Nastavniki/"
-              className="footer__column-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              YouTube
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <FooterLinks />
     </footer>
   );
 }
--- src/components/FooterLinks/FooterLinks.jsx
+++ src/components/FooterLinks/FooterLinks.jsx
@@ -1,7 +1,94 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function FooterLinks() {
-  return;
+  return (
+    <>
+      <nav className="footer__column footer__column_content_info">
+        <ul className="footer__column-list">
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              О проекте
+            </NavLink>
+          </li>
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              Календарь
+            </NavLink>
+          </li>
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              Куда пойти
+            </NavLink>
+          </li>
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              Вопросы
+            </NavLink>
+          </li>
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              Читать и смотреть
+            </NavLink>
+          </li>
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              Права детей
+            </NavLink>
+          </li>
+          <li className="footer__column-links">
+            <NavLink to="/" className="footer__column-link">
+              Истории
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <nav className="footer__column footer__column_content_social">
+        <ul className="footer__column-list">
+          <li className="footer__column-links">
+            <a
+              href="https://www.facebook.com/BigBrothers.BigSisters.Russia/"
+              className="footer__column-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+          </li>
+          <li className="footer__column-links">
+            <a
+              href="https://vk.com/big.brothers.big.sisters"
+              className="footer__column-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Vkontakte
+            </a>
+          </li>
+          <li className="footer__column-links">
+            <a
+              href="https://www.instagram.com/nastavniki_org/"
+              className="footer__column-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+          <li className="footer__column-links">
+            <a
+              href="https://www.youtube.com/user/Nastavniki/"
+              className="footer__column-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              YouTube
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default FooterLinks;
