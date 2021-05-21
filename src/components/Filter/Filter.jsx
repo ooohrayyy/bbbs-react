41ecb35e8e6c3781fa2dd47822abb8c3dcb87cbd
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-21 13:14:18 +0300

    fix: вернуть удалённые компоненты

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import FilterTag from '../FilterTag/FilterTag';
+
+function Filter() {
+  const initialItem = [
+    {
+      name: 'Все',
+      active: true,
+    },
+    {
+      name: 'Выбор наставников',
+      active: false,
+    },
+    {
+      name: 'Музеи',
+      active: false,
+    },
+    {
+      name: 'Парки',
+      active: false,
+    },
+    {
+      name: 'Театры',
+      active: false,
+    },
+    {
+      name: 'Спорт',
+      active: false,
+    },
+    {
+      name: 'Экскурсии',
+      active: false,
+    },
+    {
+      name: 'Секции',
+      active: false,
+    },
+    {
+      name: '8-10 лет',
+      active: false,
+    },
+    {
+      name: '11-13 лет',
+      active: false,
+    },
+  ];
+
+  return (
+    <div className="tags">
+      <ul className="tags__list">
+        {initialItem.map((item, i) => (
+          <FilterTag tagName={item.name} tagActive={item.active} id={i} />
+        ))}
+        <li className="tags__list-item">
+          <button className="button tags__button" type="button">
+            Выбор наставников
+          </button>
+        </li>
+        <li className="tags__list-item">
+          <button className="button tags__button" type="button">
+            Музеи
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Filter;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FooterMainContent from '../FooterMainContent/FooterMainContent';
+import FooterLinks from '../FooterLinks/FooterLinks';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <FooterMainContent />
+      <FooterLinks />
+    </footer>
+  );
+}
