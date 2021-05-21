0c4a6708a33e79cc53561dd0469186fc045a1161
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-21 13:53:05 +0300

    refactor: вынести вёрстку карточки ответа из Answers в Answer

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-function Answer() {
-  return;
+function Answer({ question, answer, rubric }) {
+  return (
+    <article className="question">
+      <h2 className="section-title question__title">{question}</h2>
+      <div className="question__wrap">
+        <p className="rubric question__rubric">{rubric}</p>
+        <button
+          className="question__show-button"
+          type="button"
+          aria-label="Показать ответ"
+          title="Показать ответ"
+        />
+      </div>
+      <div className="question__answer">
+        <p className="paragraph question__paragraph">{answer}</p>
+      </div>
+    </article>
+  );
 }
 
 export default Answer;
