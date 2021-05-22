c9881552b891f473fb28ded2380d2fd9a291946d
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-22 17:30:05 +0300

    feat: перенести логику открытия модалки с авторизацией в HeaderSignIn

--- src/components/HeaderButtons/HeaderButtons.jsx
+++ src/components/HeaderButtons/HeaderButtons.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
 
 import HeaderSearch from '../HeaderSearch/HeaderSearch';
-import Signin from '../Popups/Signin/Signin';
+import HeaderSignIn from '../HeaderSignIn/HeaderSignIn';
 
 function HeaderButtons({ toggleSearchMenu, searchOptionsClass }) {
-  const [signInModalIsOpen, setSignInModalIsOpen] = React.useState(false);
-
-  function handleSignInModal() {
-    if (signInModalIsOpen) {
-      setSignInModalIsOpen(false);
-    } else {
-      setSignInModalIsOpen(true);
-    }
-  }
-
-  function handleSignIn() {
-    console.log('Попытка входа!');
-  }
-
   return (
     <>
       <ul className="menu__button-list">
@@ -28,16 +14,9 @@ function HeaderButtons({ toggleSearchMenu, searchOptionsClass }) {
           />
         </li>
         <li className="menu__button-item">
-          <button
-            className="menu__button menu__button_type_user"
-            type="button"
-            onClick={handleSignInModal}
-            aria-label="Личный кабинет"
-            title="Личный кабинет"
-          />
+          <HeaderSignIn />
         </li>
       </ul>
-      <Signin isOpen={signInModalIsOpen} handleSignin={handleSignIn} />
     </>
   );
 }
--- src/components/HeaderSignIn/HeaderSignIn.jsx
+++ src/components/HeaderSignIn/HeaderSignIn.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import Signin from '../Popups/Signin/Signin';
+
 function HeaderSignIn() {
-  return;
+  const [signInModalIsOpen, setSignInModalIsOpen] = React.useState(false);
+
+  function handleSignInModal() {
+    console.log('Открытие окна авторизации');
+
+    if (signInModalIsOpen) {
+      setSignInModalIsOpen(false);
+    } else {
+      setSignInModalIsOpen(true);
+    }
+  }
+
+  function handleSignIn() {
+    console.log('Попытка входа!');
+  }
+
+  return (
+    <>
+      <button
+        className="menu__button menu__button_type_user"
+        type="button"
+        onClick={handleSignInModal}
+        aria-label="Личный кабинет"
+        title="Личный кабинет"
+      />
+      <Signin isOpen={signInModalIsOpen} handleSignin={handleSignIn} />
+    </>
+  );
 }
 
 export default HeaderSignIn;
