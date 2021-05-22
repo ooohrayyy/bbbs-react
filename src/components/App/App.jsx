ee16b1984012106a9a9a5b8e1b6607dff6931560
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-22 16:30:49 +0300

    feat: добавить Header и Footer в App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 
 import Header from '../Header/Header';
-// import Calendar from '../Calendar/Calendar';
-// import Main from '../Main/Main';
-// import AboutUs from '../AboutUs/AboutUs';
-// import Places from '../Places/Places';
-// import Answers from '../Answers/Answers';
-import Readings from '../Readings/Readings';
-// import Footer from '../Footer/Footer';
-
-// import answersData from '../../assets/dev-data/answersData';
-
-import './App.css';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Calendar /> */}
-      {/* <Main /> */}
-      {/* <AboutUs /> */}
-      {/* <Places /> */}
-      {/* <Answers questions={answersData} /> */}
-      <Readings />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
