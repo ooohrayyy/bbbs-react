7202088c16f50b9075ce58519f3ec5a036637ba8
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-22 16:38:34 +0300

    feat: добавить в App основные компоненты и роутинг

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+
+import Main from '../Main/Main';
+import Calendar from '../Calendar/Calendar';
+import AboutUs from '../AboutUs/AboutUs';
 
 function App() {
   return (
     <>
       <Header />
+      <Switch>
+        <Route exact path="/main">
+          <Main />
+        </Route>
+        <Route exact path="/about">
+          <AboutUs />
+        </Route>
+        <Route exact path="/calendar">
+          <Calendar />
+        </Route>
+        <Route exact path="/profile" />
+        <Route path="/">
+          <Redirect to="/main" />
+        </Route>
+      </Switch>
       <Footer />
     </>
   );
