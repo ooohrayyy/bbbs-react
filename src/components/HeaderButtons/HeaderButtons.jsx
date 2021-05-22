6760e6824b691588d8753c0a725ce539acf2d0b7
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-22 17:08:58 +0300

    feat: прописать логику открытия модалки с авторизацией в HeaderButtons

--- src/components/HeaderButtons/HeaderButtons.jsx
+++ src/components/HeaderButtons/HeaderButtons.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 
 import HeaderSearch from '../HeaderSearch/HeaderSearch';
+import Signin from '../Popups/Signin/Signin';
 
 function HeaderButtons({ toggleSearchMenu, searchOptionsClass }) {
+  const [signInModalIsOpen, setSignInModalIsOpen] = React.useState(false);
+
+  function handleSignInModal() {
+    if (signInModalIsOpen) {
+      setSignInModalIsOpen(false);
+    } else {
+      setSignInModalIsOpen(true);
+    }
+  }
+
+  function handleSignIn() {
+    console.log('Попытка входа!');
+  }
+
   return (
-    <ul className="menu__button-list">
-      <li className="menu__button-item">
-        <HeaderSearch
-          toggleSearchMenu={toggleSearchMenu}
-          searchOptionsClass={searchOptionsClass}
-        />
-      </li>
-      <li className="menu__button-item">
-        <button
-          className="menu__button menu__button_type_user"
-          type="button"
-          aria-label="Личный кабинет"
-          title="Личный кабинет"
-        />
-      </li>
-    </ul>
+    <>
+      <ul className="menu__button-list">
+        <li className="menu__button-item">
+          <HeaderSearch
+            toggleSearchMenu={toggleSearchMenu}
+            searchOptionsClass={searchOptionsClass}
+          />
+        </li>
+        <li className="menu__button-item">
+          <button
+            className="menu__button menu__button_type_user"
+            type="button"
+            onClick={handleSignInModal}
+            aria-label="Личный кабинет"
+            title="Личный кабинет"
+          />
+        </li>
+      </ul>
+      <Signin isOpen={signInModalIsOpen} handleSignin={handleSignIn} />
+    </>
   );
 }
 
