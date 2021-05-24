c268fdf733263edb9d226e0dc259a06fd8dfa3ce
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-24 14:51:02 +0300

    feat: добавить данные для отображения ReadingsVideoCard

---
+++ src/assets/dev-data/ReadingsData/ReadingsVideoData/readingsVideoCardsData.js
@@ -0,0 +1,46 @@
+const readingsVideoCardsData = [
+  {
+    cardTitle: 'Меняется смысл – меняется жизнь',
+    cardCaption: 'Прямой эфир с актрисой театра и кино Эмилией Спивак',
+    cardPreview:
+      '../../assets/dev-data/ReadingsData/ReadingsVideoData/CardImages/video-ex-1.png',
+    cardDuration: '4:37',
+  },
+  {
+    cardTitle: 'Что в силах волонтёра-наставника?',
+    cardCaption: 'Иван Рустаев, волонтер-наставник',
+    cardPreview:
+      '../../assets/dev-data/ReadingsData/ReadingsVideoData/CardImages/video-ex-2.png',
+    cardDuration: '4:37',
+  },
+  {
+    cardTitle: 'Творчество, наставничество, дети',
+    cardCaption: 'Виктория Ямина, Виктория Ямина, волонтёры',
+    cardPreview:
+      '../../assets/dev-data/ReadingsData/ReadingsVideoData/CardImages/video-ex-3.png',
+    cardDuration: '4:37',
+  },
+  {
+    cardTitle: 'Дружба – меняет мир',
+    cardCaption: 'Прямой эфир с актрисой театра и кино Эмилией Спивак',
+    cardPreview:
+      '../../assets/dev-data/ReadingsData/ReadingsVideoData/CardImages/video-ex-4.png',
+    cardDuration: '4:37',
+  },
+  {
+    cardTitle: 'Меняется смысл – меняется жизнь',
+    cardCaption: 'Прямой эфир с актрисой театра и кино Эмилией Спивак',
+    cardPreview:
+      '../../assets/dev-data/ReadingsData/ReadingsVideoData/CardImages/video-ex-1.png',
+    cardDuration: '4:37',
+  },
+  {
+    cardTitle: 'Что в силах волонтёра-наставника?',
+    cardCaption: 'Иван Рустаев, волонтер-наставник',
+    cardPreview:
+      '../../assets/dev-data/ReadingsData/ReadingsVideoData/CardImages/video-ex-2.png',
+    cardDuration: '4:37',
+  },
+];
+
+export default readingsVideoCardsData;
