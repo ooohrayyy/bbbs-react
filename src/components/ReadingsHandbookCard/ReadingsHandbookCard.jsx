b8cf37256500aa0fb6a1bc90bb1d95bd8cc46b64
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-24 13:25:56 +0300

    feat: прописать логику ReadingsHandbookCard

--- src/components/ReadingsHandbookCard/ReadingsHandbookCard.jsx
+++ src/components/ReadingsHandbookCard/ReadingsHandbookCard.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-function ReadingsHandbookCard() {
-  return;
+function ReadingsHandbookCard({
+  cardTitle,
+  cardImage,
+  cardAlt,
+  cardLink,
+  cardStyle,
+}) {
+  let cardModificator;
+
+  switch (cardStyle) {
+    case 'square':
+      cardModificator = 'card_form_square';
+      break;
+    case 'circle':
+      cardModificator = 'card_form_circle';
+      break;
+    case 'arc':
+      cardModificator = 'card_form_arch';
+      break;
+    default:
+      cardModificator = '';
+  }
+
+  return (
+    <article className="preview__card catalog-card">
+      <div className={`card ${cardModificator} rights__card`}>
+        <a href={cardLink} className="rights__link">
+          <img
+            src={cardImage}
+            alt={cardAlt || cardTitle}
+            className="catalog-card__image"
+          />
+        </a>
+      </div>
+      <h2 className="section-title catalog-card__title">{cardTitle}</h2>
+    </article>
+  );
 }
 
 export default ReadingsHandbookCard;
