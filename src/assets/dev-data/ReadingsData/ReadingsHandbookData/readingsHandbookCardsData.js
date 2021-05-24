d0b3cc065d83dc267ad119a80b80ba669802f290
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-24 13:34:41 +0300

    feat: добавить данные для отображения ReadingsHandbookCard

---
+++ src/assets/dev-data/ReadingsData/ReadingsHandbookData/readingsHandbookCardsData.js
@@ -0,0 +1,46 @@
+const readingsHandbookCardsData = [
+  {
+    cardTitle: 'Психологические особенности детей-сирот',
+    cardImage: './CardImages/catalog-hulk-boys.jpg',
+    cardAlt: 'Психологические особенности детей-сирот',
+    cardLink: './articles',
+    cardStyle: 'square',
+  },
+  {
+    cardTitle: 'Привязанность',
+    cardImage: './CardImages/catalog-coffee.jpg',
+    cardAlt: 'Привязанность',
+    cardLink: './articles',
+    cardStyle: 'circle',
+  },
+  {
+    cardTitle: 'Особенности социально дезадаптивных детей',
+    cardImage: './CardImages/catalog-beach.jpg',
+    cardAlt: 'Особенности социально дезадаптивных детей',
+    cardLink: './articles',
+    cardStyle: 'arc',
+  },
+  {
+    cardTitle: 'Социальная адаптация',
+    cardImage: './CardImages/catalog-coffee.jpg',
+    cardAlt: 'Социальная адаптация',
+    cardLink: './articles',
+    cardStyle: 'circle',
+  },
+  {
+    cardTitle: 'Социально дезадаптивные дети',
+    cardImage: './CardImages/catalog-blue-hand.jpg',
+    cardAlt: 'Социально дезадаптивные дети',
+    cardLink: './articles',
+    cardStyle: 'arc',
+  },
+  {
+    cardTitle: 'Проявление агрессии у детей-сирот',
+    cardImage: './CardImages/catalog-spear.jpg',
+    cardAlt: 'Проявление агрессии у детей-сирот',
+    cardLink: './articles',
+    cardStyle: 'square',
+  },
+];
+
+export default readingsHandbookCardsData;
