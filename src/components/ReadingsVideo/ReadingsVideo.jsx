a9ed2cab56c08501934dcba00a4723256585d2c1
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-24 14:56:13 +0300

    feat: переписать логику ReadingsVideo

--- src/components/ReadingsVideo/ReadingsVideo.jsx
+++ src/components/ReadingsVideo/ReadingsVideo.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import ReadingsButtons from '../ReadingsButtons/ReadingsButtons';
+import ReadingsVideoCard from '../ReadingsVideoCard/ReadingsVideoCard';
+
+import readingsVideoCardsData from '../../assets/dev-data/ReadingsData/ReadingsVideoData/readingsVideoCardsData';
 
 function ReadingsVideo() {
   return (
@@ -13,149 +16,15 @@ function ReadingsVideo() {
           <ReadingsButtons />
         </div>
         <div className="preview__row">
-          <article className="preview__card card card_content_video">
-            <div className="video">
-              <img
-                src="./images/video/video-ex-1.png"
-                alt="Превью видео"
-                className="video__img"
-              />
-              <p className="video__duration paragraph">4:37</p>
-            </div>
-            <div className="card__video-info">
-              <div className="card__title-wrap">
-                <h2 className="section-title card__title">
-                  Меняется смысл – меняется жизнь
-                </h2>
-                <p className="caption card__title-caption">
-                  Прямой эфир с актрисой театра и кино Эмилией Спивак
-                </p>
-              </div>
-              <a href="#" className="link card__link link_action_open-video">
-                смотреть видео
-              </a>
-            </div>
-          </article>
-
-          <article className="preview__card card card_content_video">
-            <div className="video">
-              <img
-                src="./images/video/video-ex-2.png"
-                alt="Превью видео"
-                className="video__img"
-              />
-              <p className="video__duration paragraph">4:37</p>
-            </div>
-            <div className="card__video-info">
-              <div className="card__title-wrap">
-                <h2 className="section-title card__title">
-                  Что в силах волонтера-наставника?
-                </h2>
-                <p className="caption card__title-caption">
-                  Иван Рустаев, волонтер-наставник
-                </p>
-              </div>
-              <a href="#" className="link card__link link_action_open-video">
-                смотреть видео
-              </a>
-            </div>
-          </article>
-
-          <article className="preview__card card card_content_video">
-            <div className="video">
-              <img
-                src="./images/video/video-ex-3.png"
-                alt="Превью видео"
-                className="video__img"
-              />
-              <p className="video__duration paragraph">4:37</p>
-            </div>
-            <div className="card__video-info">
-              <div className="card__title-wrap">
-                <h2 className="section-title card__title">
-                  Творчество, наставничество, дети
-                </h2>
-                <p className="caption card__title-caption">
-                  Виктория Ямина, Виктория Ямина, волонтеры
-                </p>
-              </div>
-              <a href="#" className="link card__link link_action_open-video">
-                смотреть видео
-              </a>
-            </div>
-          </article>
-
-          <article className="preview__card card card_content_video">
-            <div className="video">
-              <img
-                src="./images/video/video-ex-4.png"
-                alt="Превью видео"
-                className="video__img"
-              />
-              <p className="video__duration paragraph">4:37</p>
-            </div>
-            <div className="card__video-info">
-              <div className="card__title-wrap">
-                <h2 className="section-title card__title">
-                  Дружба – меняет мир
-                </h2>
-                <p className="caption card__title-caption">
-                  Прямой эфир с актрисой театра и кино Эмилией Спивак
-                </p>
-              </div>
-              <a href="#" className="link card__link link_action_open-video">
-                смотреть видео
-              </a>
-            </div>
-          </article>
-
-          <article className="preview__card card card_content_video">
-            <div className="video">
-              <img
-                src="./images/video/video-ex-1.png"
-                alt="Превью видео"
-                className="video__img"
-              />
-              <p className="video__duration paragraph">4:37</p>
-            </div>
-            <div className="card__video-info">
-              <div className="card__title-wrap">
-                <h2 className="section-title card__title">
-                  Меняется смысл – меняется жизнь
-                </h2>
-                <p className="caption card__title-caption">
-                  Прямой эфир с актрисой театра и кино Эмилией Спивак
-                </p>
-              </div>
-              <a href="#" className="link card__link link_action_open-video">
-                смотреть видео
-              </a>
-            </div>
-          </article>
-
-          <article className="preview__card card card_content_video">
-            <div className="video">
-              <img
-                src="./images/video/video-ex-2.png"
-                alt="Превью видео"
-                className="video__img"
-              />
-              <p className="video__duration paragraph">4:37</p>
-            </div>
-            <div className="card__video-info">
-              <div className="card__title-wrap">
-                <h2 className="section-title card__title">
-                  Что в силах волонтера-наставника?
-                </h2>
-                <p className="caption card__title-caption">
-                  Иван Рустаев, волонтер-наставник
-                </p>
-              </div>
-              <a href="#" className="link card__link link_action_open-video">
-                смотреть видео
-              </a>
-            </div>
-          </article>
+          {readingsVideoCardsData.map((item, index) => (
+            <ReadingsVideoCard
+              cardTitle={item.cardTitle}
+              cardCaption={item.cardCaption}
+              cardPreview={item.cardPreview}
+              cardDuration={item.cardDuration}
+              key={index}
+            />
+          ))}
         </div>
       </section>
     </>
