d712efe674345d2ae4d3486c96ca2f589dc132a0
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-05-26 12:30:30 +0400

    feat: показывать поп-апы при нажатии на карточки событий в ЛК

--- src/components/UserRegistredEvent/UserRegistredEvent.jsx
+++ src/components/UserRegistredEvent/UserRegistredEvent.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import { React, useState } from 'react';
+
+import DelRecord from '../Popups/DelRecord/DelRecord';
 
 function UserRegistredEvent({ title, startDayMonth, startMonth }) {
+  const [isDelRecPopupOpen, setIsDelRecPopupOpen] = useState(false);
+
+  function handleRegEventClick() {
+    setIsDelRecPopupOpen(true);
+  }
+
+  function closeRegEventPopup() {
+    setIsDelRecPopupOpen(false);
+  }
+
   return (
-    <li className="personal-area__reg-event">
-      <div className="personal-area__reg-event-date">
-        <span className="personal-area__reg-event-day">{startDayMonth}</span>
-        <span className="personal-area__reg-event-month">{startMonth}</span>
-      </div>
-      <p className="personal-area__reg-event-description">{title}</p>
-    </li>
+    <>
+      <li className="personal-area__reg-event" onClick={handleRegEventClick}>
+        <div className="personal-area__reg-event-date">
+          <span className="personal-area__reg-event-day">{startDayMonth}</span>
+          <span className="personal-area__reg-event-month">{startMonth}</span>
+        </div>
+        <p className="personal-area__reg-event-description">{title}</p>
+      </li>
+      <DelRecord
+        isOpen={isDelRecPopupOpen}
+        title={title}
+        handleClose={closeRegEventPopup}
+        handleConfirm={closeRegEventPopup}
+      />
+    </>
   );
 }
 
