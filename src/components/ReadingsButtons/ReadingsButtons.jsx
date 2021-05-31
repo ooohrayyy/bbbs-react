b0654cf8ad0815ecf3034b8462127ad9d134e714
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-31 21:10:42 +0300

    feat: добавить и применить хук useLongPress

--- src/components/ReadingsButtons/ReadingsButtons.jsx
+++ src/components/ReadingsButtons/ReadingsButtons.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import useLongPress from '../../utils/useLongPress';
+
 function ReadingsButtons({ onClickLeft, onClickRight }) {
+  const leftLongPress = useLongPress(onClickLeft, 100);
+  const rightLongPress = useLongPress(onClickRight, 100);
+
   return (
     <div className="preview__buttons">
       <button
@@ -8,12 +13,22 @@ function ReadingsButtons({ onClickLeft, onClickRight }) {
         type="button"
         label="left"
         onClick={onClickLeft}
+        onMouseDown={leftLongPress.onMouseDown}
+        onMouseUp={leftLongPress.onMouseUp}
+        onMouseLeave={leftLongPress.onMouseLeave}
+        onTouchStart={leftLongPress.onTouchStart}
+        onTouchEnd={leftLongPress.onTouchEnd}
       />
       <button
         className="preview__button preview__button_right"
         type="button"
         label="right"
         onClick={onClickRight}
+        onMouseDown={rightLongPress.onMouseDown}
+        onMouseUp={rightLongPress.onMouseUp}
+        onMouseLeave={rightLongPress.onMouseLeave}
+        onTouchStart={rightLongPress.onTouchStart}
+        onTouchEnd={rightLongPress.onTouchEnd}
       />
     </div>
   );
