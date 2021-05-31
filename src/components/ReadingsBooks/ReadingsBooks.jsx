54a7240f36eadd23d49761699ed1b8c7120f418a
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-31 20:10:51 +0300

    feat: переписать логику ReadingsBooks

--- src/components/ReadingsBooks/ReadingsBooks.jsx
+++ src/components/ReadingsBooks/ReadingsBooks.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import ReadingsButtons from '../ReadingsButtons/ReadingsButtons';
+import ReadingsBookCard from '../ReadingsBookCard/ReadingsBookCard';
+
+import readingsBooksCardsData from '../../assets/dev-data/ReadingsData/ReadingsBooksData/readingsBooksCardsData';
 
 function ReadingsBooks() {
   return (
@@ -13,233 +16,16 @@ function ReadingsBooks() {
           <ReadingsButtons />
         </div>
         <div className="preview__row">
-          <article className="preview__card card-container">
-            <div className="card card_content_book">
-              <div className="book book_color_pink">
-                <h2 className="section-title book__title">
-                  Жизнь после утраты
-                </h2>
-                <div className="book__info">
-                  <p className="caption book__author">
-                    Волкан Вамик, Зинтл Элизабет
-                  </p>
-                  <p className="caption book__year">2011 год</p>
-                </div>
-              </div>
-            </div>
-            <div className="card card_content_annotation">
-              <div className="card__content">
-                <div className="card__annotation">
-                  <p className="paragraph card__paragraph">
-                    Аннотация книги, статьи, фильма в несколько абзацев. В тот
-                    момент, как ребёнок научился говорить, и не одно слово,
-                    а задавать бесконечное количество вопросов, жизнь меняется.
-                    Вы будете не понимать друг друга, потом понимать чуть лучше
-                    и, в конце концов, научитесь даже шутить. В тот момент, как
-                    ребёнок научился говорить, и не одно слово, а задавать
-                    бесконечное количество вопросов, жизнь меняется.
-                  </p>
-                  <p className="paragraph card__paragraph">
-                    Аннотация статьи в несколько абзацев. Аннотация статьи в
-                    несколько абзацев. В тот момент, как ребёнок научился
-                    говорить, и не одно слово, а задавать бесконечное количество
-                    вопросов, жизнь меняется. Вы будете не понимать друг друга,
-                    потом понимать чуть лучше и, в конце концов, научитесь даже
-                    шутить. Аннотация статьи в несколько абзацев.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </article>
-
-          <article className="preview__card card-container">
-            <div className="card card_content_book">
-              <div className="book book_color_blue">
-                <h2 className="section-title book__title">
-                  Жизнь после утраты. Психология горевания
-                </h2>
-                <div className="book__info">
-                  <p className="caption book__author">Волкан Вамик,</p>
-                  <p className="caption book__year">2011 год</p>
-                </div>
-              </div>
-            </div>
-            <div className="card card_content_annotation">
-              <div className="card__content">
-                <div className="card__annotation">
-                  <p className="paragraph card__paragraph">
-                    Аннотация книги, статьи, фильма в несколько абзацев. В тот
-                    момент, как ребёнок научился говорить, и не одно слово,
-                    а задавать бесконечное количество вопросов, жизнь меняется.
-                    Вы будете не понимать друг друга, потом понимать чуть лучше
-                    и, в конце концов, научитесь даже шутить. В тот момент, как
-                    ребёнок научился говорить, и не одно слово, а задавать
-                    бесконечное количество вопросов, жизнь меняется.
-                  </p>
-                  <p className="paragraph card__paragraph">
-                    Аннотация статьи в несколько абзацев. Аннотация статьи в
-                    несколько абзацев. В тот момент, как ребёнок научился
-                    говорить, и не одно слово, а задавать бесконечное количество
-                    вопросов, жизнь меняется. Вы будете не понимать друг друга,
-                    потом понимать чуть лучше и, в конце концов, научитесь даже
-                    шутить. Аннотация статьи в несколько абзацев.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </article>
-
-          <article className="preview__card card-container">
-            <div className="card card_content_book">
-              <div className="book book_color_pink">
-                <h2 className="section-title book__title">
-                  Жизнь после утраты
-                </h2>
-                <div className="book__info">
-                  <p className="caption book__author">
-                    Волкан Вамик, Зинтл Элизабет
-                  </p>
-                  <p className="caption book__year">2011 год</p>
-                </div>
-              </div>
-            </div>
-            <div className="card card_content_annotation">
-              <div className="card__content">
-                <div className="card__annotation">
-                  <p className="paragraph card__paragraph">
-                    Аннотация книги, статьи, фильма в несколько абзацев. В тот
-                    момент, как ребёнок научился говорить, и не одно слово,
-                    а задавать бесконечное количество вопросов, жизнь меняется.
-                    Вы будете не понимать друг друга, потом понимать чуть лучше
-                    и, в конце концов, научитесь даже шутить. В тот момент, как
-                    ребёнок научился говорить, и не одно слово, а задавать
-                    бесконечное количество вопросов, жизнь меняется.
-                  </p>
-                  <p className="paragraph card__paragraph">
-                    Аннотация статьи в несколько абзацев. Аннотация статьи в
-                    несколько абзацев. В тот момент, как ребёнок научился
-                    говорить, и не одно слово, а задавать бесконечное количество
-                    вопросов, жизнь меняется. Вы будете не понимать друг друга,
-                    потом понимать чуть лучше и, в конце концов, научитесь даже
-                    шутить. Аннотация статьи в несколько абзацев.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </article>
-
-          <article className="preview__card card-container">
-            <div className="card card_content_book">
-              <div className="book book_color_blue">
-                <h2 className="section-title book__title">
-                  Жизнь после утраты. Психология горевания
-                </h2>
-                <div className="book__info">
-                  <p className="caption book__author">Волкан Вамик,</p>
-                  <p className="caption book__year">2011 год</p>
-                </div>
-              </div>
-            </div>
-            <div className="card card_content_annotation">
-              <div className="card__content">
-                <div className="card__annotation">
-                  <p className="paragraph card__paragraph">
-                    Аннотация книги, статьи, фильма в несколько абзацев. В тот
-                    момент, как ребёнок научился говорить, и не одно слово,
-                    а задавать бесконечное количество вопросов, жизнь меняется.
-                    Вы будете не понимать друг друга, потом понимать чуть лучше
-                    и, в конце концов, научитесь даже шутить. В тот момент, как
-                    ребёнок научился говорить, и не одно слово, а задавать
-                    бесконечное количество вопросов, жизнь меняется.
-                  </p>
-                  <p className="paragraph card__paragraph">
-                    Аннотация статьи в несколько абзацев. Аннотация статьи в
-                    несколько абзацев. В тот момент, как ребёнок научился
-                    говорить, и не одно слово, а задавать бесконечное количество
-                    вопросов, жизнь меняется. Вы будете не понимать друг друга,
-                    потом понимать чуть лучше и, в конце концов, научитесь даже
-                    шутить. Аннотация статьи в несколько абзацев.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </article>
-
-          <article className="preview__card card-container">
-            <div className="card card_content_book">
-              <div className="book book_color_pink">
-                <h2 className="section-title book__title">
-                  Жизнь после утраты
-                </h2>
-                <div className="book__info">
-                  <p className="caption book__author">
-                    Волкан Вамик, Зинтл Элизабет
-                  </p>
-                  <p className="caption book__year">2011 год</p>
-                </div>
-              </div>
-            </div>
-            <div className="card card_content_annotation">
-              <div className="card__content">
-                <div className="card__annotation">
-                  <p className="paragraph card__paragraph">
-                    Аннотация книги, статьи, фильма в несколько абзацев. В тот
-                    момент, как ребёнок научился говорить, и не одно слово,
-                    а задавать бесконечное количество вопросов, жизнь меняется.
-                    Вы будете не понимать друг друга, потом понимать чуть лучше
-                    и, в конце концов, научитесь даже шутить. В тот момент, как
-                    ребёнок научился говорить, и не одно слово, а задавать
-                    бесконечное количество вопросов, жизнь меняется.
-                  </p>
-                  <p className="paragraph card__paragraph">
-                    Аннотация статьи в несколько абзацев. Аннотация статьи в
-                    несколько абзацев. В тот момент, как ребёнок научился
-                    говорить, и не одно слово, а задавать бесконечное количество
-                    вопросов, жизнь меняется. Вы будете не понимать друг друга,
-                    потом понимать чуть лучше и, в конце концов, научитесь даже
-                    шутить. Аннотация статьи в несколько абзацев.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </article>
-
-          <article className="preview__card card-container">
-            <div className="card card_content_book">
-              <div className="book book_color_blue">
-                <h2 className="section-title book__title">
-                  Жизнь после утраты. Психология горевания
-                </h2>
-                <div className="book__info">
-                  <p className="caption book__author">Волкан Вамик,</p>
-                  <p className="caption book__year">2011 год</p>
-                </div>
-              </div>
-            </div>
-            <div className="card card_content_annotation">
-              <div className="card__content">
-                <div className="card__annotation">
-                  <p className="paragraph card__paragraph">
-                    Аннотация книги, статьи, фильма в несколько абзацев. В тот
-                    момент, как ребёнок научился говорить, и не одно слово,
-                    а задавать бесконечное количество вопросов, жизнь меняется.
-                    Вы будете не понимать друг друга, потом понимать чуть лучше
-                    и, в конце концов, научитесь даже шутить. В тот момент, как
-                    ребёнок научился говорить, и не одно слово, а задавать
-                    бесконечное количество вопросов, жизнь меняется.
-                  </p>
-                  <p className="paragraph card__paragraph">
-                    Аннотация статьи в несколько абзацев. Аннотация статьи в
-                    несколько абзацев. В тот момент, как ребёнок научился
-                    говорить, и не одно слово, а задавать бесконечное количество
-                    вопросов, жизнь меняется. Вы будете не понимать друг друга,
-                    потом понимать чуть лучше и, в конце концов, научитесь даже
-                    шутить. Аннотация статьи в несколько абзацев.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </article>
+          {readingsBooksCardsData.map((book, index) => (
+            <ReadingsBookCard
+              title={book.title}
+              author={book.author}
+              year={book.year}
+              annotation={book.annotation}
+              color={book.color}
+              key={index}
+            />
+          ))}
         </div>
       </section>
     </>
