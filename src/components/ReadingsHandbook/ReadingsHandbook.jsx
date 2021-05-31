02f091f5f51ebd8b67e257132f5bc32a66b97ce3
Boris Nikitashov <ooohrayyy@gmail.com>
2021-05-31 20:41:26 +0300

    feat: добавить скролл по нажатию кнопок в ReadingsHandbook

--- src/components/ReadingsHandbook/ReadingsHandbook.jsx
+++ src/components/ReadingsHandbook/ReadingsHandbook.jsx
@@ -6,15 +6,31 @@ import ReadingsHandbookCard from '../ReadingsHandbookCard/ReadingsHandbookCard';
 import readingsHandbookCardsData from '../../assets/dev-data/ReadingsData/ReadingsHandbookData/readingsHandbookCardsData';
 
 function ReadingsHandbook() {
+  const [containerPosition, setContainerPosition] = React.useState(0);
+
+  const containerRef = React.useRef(null);
+
+  function scrollRight() {
+    containerRef.current.scroll(containerPosition + 300, 0);
+
+    setContainerPosition(containerPosition + 300);
+  }
+
+  function scrollLeft() {
+    containerRef.current.scroll(containerPosition - 300, 0);
+
+    setContainerPosition(containerPosition - 300);
+  }
+
   return (
     <section className="preview page__section">
       <div className="preview__title-wrap">
         <a href="./catalog.html" className="link">
           <h3 className="chapter-title chapter-title_clickable">Справочник</h3>
         </a>
-        <ReadingsButtons />
+        <ReadingsButtons onClickRight={scrollRight} onClickLeft={scrollLeft} />
       </div>
-      <div className="preview__row">
+      <div className="preview__row" ref={containerRef}>
         {readingsHandbookCardsData.map((item, index) => (
           <ReadingsHandbookCard
             cardTitle={item.cardTitle}
