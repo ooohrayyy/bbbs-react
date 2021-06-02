98573623f32610227c2b945c7b7f3975cfe9978f
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-06-02 18:58:46 +0400

    fix: временно убрать дочерний компонент для раздела о правах детей

--- src/components/Rights/Rights.jsx
+++ src/components/Rights/Rights.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import arrow from '../../images/svg/arrow-right.svg';
 
 function Rights() {
@@ -143,7 +142,5 @@ function Rights() {
     </>
   );
 }
-
-// Rights.propTypes = {};
 
 export default Rights;
