7fbbc02f44b6f095bd222dff018bd0c665d64bc7
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-06-02 23:08:06 +0400

    feat: добавить данные и компонент для карточек из раздела о правах детей

---
+++ src/assets/dev-data/rightsCardsData.js
@@ -0,0 +1,100 @@
+const rightsCardsData = [
+  {
+    cardTitle: 'Пенсионное обеспечение для детей-сирот',
+    cardLink: './rights-article',
+    cardForm: 'square',
+    cardColor: 'yellow',
+  },
+  {
+    cardTitle: 'Льготы детей на жильё',
+    cardLink: './rights-article',
+    cardForm: 'circle',
+    cardColor: 'green',
+  },
+  {
+    cardTitle: 'Трудоустройство для детей инвалидов',
+    cardLink: './rights-article',
+    cardForm: 'arch',
+    cardColor: 'yellow',
+  },
+  {
+    cardTitle: 'Пенсионное обеспечение для детей-сирот',
+    cardLink: './rights-article',
+    cardForm: 'circle',
+    cardColor: 'pink',
+  },
+  {
+    cardTitle: 'Жилье для выпускников детских домов',
+    cardLink: './rights-article',
+    cardForm: 'arch',
+    cardColor: 'blue',
+  },
+  {
+    cardTitle: 'Трудоустройство для выпускников детских домов',
+    cardLink: './rights-article',
+    cardForm: 'square',
+    cardColor: 'pink',
+  },
+  {
+    cardTitle: 'Трудоустройство для детей инвалидов',
+    cardLink: './rights-article',
+    cardForm: 'arch',
+    cardColor: 'green',
+  },
+  {
+    cardTitle: 'Льготы детей на жильё',
+    cardLink: './rights-article',
+    cardForm: 'square',
+    cardColor: 'yellow',
+  },
+  {
+    cardTitle: 'Пенсионное обеспечение для детей-сирот',
+    cardLink: './rights-article',
+    cardForm: 'circle',
+    cardColor: 'green',
+  },
+  {
+    cardTitle: 'Льготы детей на жильё',
+    cardLink: './rights-article',
+    cardForm: 'square',
+    cardColor: 'blue',
+  },
+  {
+    cardTitle: 'Трудоустройство для детей инвалидов',
+    cardLink: './rights-article',
+    cardForm: 'circle',
+    cardColor: 'pink',
+  },
+  {
+    cardTitle: 'Пенсионное обеспечение для детей-сирот',
+    cardLink: './rights-article',
+    cardForm: 'arch',
+    cardColor: 'blue',
+  },
+  {
+    cardTitle: 'Льготы детей на жильё',
+    cardLink: './rights-article',
+    cardForm: 'circle',
+    cardColor: 'yellow',
+  },
+  {
+    cardTitle: 'Пенсионное обеспечение для детей-сирот',
+    cardLink: './rights-article',
+    cardForm: 'arch',
+    cardColor: 'green',
+  },
+  {
+    cardTitle: 'Льготы детей на жильё',
+    cardLink: './rights-article',
+    cardForm: 'square',
+    cardColor: 'yellow',
+  },
+  {
+    cardTitle: 'Пенсионное обеспечение для детей-сирот',
+    cardLink: './rights-article',
+    cardForm: 'arch',
+    cardColor: 'pink',
+  },
+];
+
+export default rightsCardsData;
---
+++ src/components/Rights/RightsCard/RightsCard.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function RightsCard({ cardTitle, cardLink, cardForm, cardColor }) {
+  let form;
+  let color;
+
+  switch (cardForm) {
+    case 'square':
+      form = 'card_form_square';
+      break;
+    case 'circle':
+      form = 'card_form_circle';
+      break;
+    case 'arch':
+      form = 'card_form_arch';
+      break;
+    default:
+      form = '';
+  }
+
+  switch (cardColor) {
+    case 'green':
+      color = 'card_color_green';
+      break;
+    case 'yellow':
+      color = 'card_color_yellow';
+      break;
+    case 'blue':
+      color = 'card_color_blue';
+      break;
+    case 'pink':
+      color = 'card_color_pink';
+      break;
+    default:
+      color = '';
+  }
+
+  return (
+    <div className="catalog-card card-pagination card-pagination_type_shapes">
+      <div className={`card ${color} ${form} rights__card`}>
+        <Link className="rights__link" to={cardLink}>
+          <h2 className="section-title">{cardTitle}</h2>
+          <p className="rubric rights__rubric">рубрика</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+RightsCard.propTypes = {
+  cardTitle: PropTypes.string,
+  cardLink: PropTypes.string,
+  cardForm: PropTypes.string,
+  cardColor: PropTypes.string,
+};
+
+export default RightsCard;
